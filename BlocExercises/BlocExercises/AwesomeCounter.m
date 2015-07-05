54e6c0cdc38817e730d88dc2aa9b73e3e949fbf1
Henry <henrysmithjr@me.com>
2015-07-05 19:08:26 -0400

    Assignment 12 completed

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,7 +12,24 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @"";
+    NSMutableString *stringToManipulate = [@"" mutableCopy];
+    BOOL numIsLow = number < otherNumber ? YES : NO;
+    
+    if(numIsLow){
+        for(NSInteger i = number; i <= otherNumber; i++){
+            [stringToManipulate appendFormat:@"%ld",(long)i];
+        }
+    }
+    else{
+        for(NSInteger i = otherNumber; i <= number; i++){
+            [stringToManipulate appendFormat:@"%ld",(long)i];
+        }
+    }
+    
+    NSString *stringtoReturn = stringToManipulate;
+    
+    return stringtoReturn;
+    
 }
 
 @end
